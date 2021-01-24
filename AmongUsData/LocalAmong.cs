6cb4170ddf0ba7651288a4d9955f67809db75fa2
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-24 12:45:48 +0300

    New page

---
+++ AmongUsData/LocalAmong.cs
@@ -0,0 +1,7 @@
+<# dsml># 
+       Scriptip.create(command.scriptdata.data.amongus.(script.js.datascript.amongus(script.
+       Scriptip.create(command.scriptdata.data/localamongus == data == among us launcher
+     Login.dev(datascript.commander.data(script(data/complex.data.
+               Dev.data/nanocomplex.java/data.yml(hosting.data.script="true"
+         
+           CreateScript.ip(data.script.command/local.github(localink.datascripter.script
