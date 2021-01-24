eabeecbc0eca51a7d864fa882f62a3c9c19d20d2
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-24 13:01:20 +0300

    ModMaker

---
+++ ModsCreator/ModMaker.cs
@@ -0,0 +1,11 @@
+#script.data.;complex.data/impostorserver.modmaker.createGameMode.js.(@local;script
+#script;data.complex fan.data(complex.AmongUs == launcher.data/modmaker.role.(createrole)).paint.
+#Killbutton.paint.Command.data.(xml.mll.command == launcher="Among-us"
+#createButton.command.cs(is.script.data/(data)
+#programcodes.command.cs(is data="Among-us"
+             
+             Script="data.among.us./datascript.creator/data
+        
+       Scriptbutton.local/Among-us(programming).updateto.commands.rfdml
+       JavaButton.scripter./locals.script(nameddata.beta)
+Locals.data
