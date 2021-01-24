361dba283f13818c7d71a69c02ac20d820faef93
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-24 12:09:28 +0300

    Among us codes update

--- Among.js
+++ Among.js
@@ -7,3 +7,22 @@
       
      <Systems.server(region.ip(script.html(localweb.data(cs.ip)
         AmongUsGame.if(localdata) == script[], cs.10.bot
+     
+        if(localdata == gamescript.developer(license.bot.1.2.10.bit(data)
+        if(updates == root.script.(license.ip[token]
+      
+     <Systems.local(data.[script.ip(region.ipadresses="otoip(activate.program(script)
+   
+              LocalLauncher.ip(scripter.data.ip(region(among us == server.privatehost(script)
+              LauncherData.ip(scripter.data(region.ip,createserver.regionscript.data.cs(script)
+              Githublauncher.ip(among us == impostor == language.ip
+           if(yourdata.script(closedata.adminscript(datacomplex.ip[], script
+           if(data.scripter.nanodata(script == among us 
+           Root.ip(scriptdata.(nanoscript(yourdata.github.impostor(launcher == script.data(yourdata.complex
+           Root.ip(scriptdata.(nanoscript(data.complex(impostor == login-impostor.ip
+         
+        @spacemafia.(script.itsdata
+        @ipdata.(script.itipdata[localserver.dara.githubpage.resip.(local.ip[script.login
+        Host.script]
+        @data.script(data.vix(github.data.controller
+        @script.yourdata(spacemafia.chatcontroller(ip).fakemesage.ip(script)
