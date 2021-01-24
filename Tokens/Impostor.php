e3bd6a3a81f0cca038e1dd1ef69b68b4196ea8d4
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-24 16:21:21 +0300

    Impostor.php

---
+++ Tokens/Impostor.php
@@ -0,0 +1,13 @@
+<Token> 
+      <typer>
+    //PHP:Hostinger//
+        HostingScript(data.localserver.doc(data)
+   
+      @$AmongUsHostinger="doc-localhost:8158
+      @$AmongUsDataHosting="doc_type<Localhost>api.data
+      @$AmongUsModder="Controllerchat.server="ip.createip"
+      @$ImpostorLanguage="docsapi-hostinger" ipadresses.local(createServer.ip)
+      
+
+      
+    
