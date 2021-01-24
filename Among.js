4cafa9826bd8783b4cac35245ec7784043fa21e6
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-24 11:45:48 +0300

    Among.js Programming

---
+++ Among.js
@@ -0,0 +1,9 @@
+<script
+      amongus.PlayerController.js(script == localdata.script
+      cs.playercontroller(script == if.local(script).inputdata == among us
+       
+    <system.ip(localdata.amongus.chacater(game.ingame(develop?="script"
+       If(script == among us.data(local == is controller.data(local)
+      
+     <Systems.server(region.ip(script.html(localweb.data(cs.ip)
+        AmongUsGame.if(localdata) == script[], cs.10.bot
