3e916e02b41da293e9dc2a22f528d34bb5a19144
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-24 16:29:54 +0300

    Api.php

---
+++ Tokens/Api.php
@@ -0,0 +1,15 @@
+<Typer>
+     <Typer> Localdata <Typer>
+                LocalHost.typer(data)
+     Api:cloud, formation="typer"
+            Cloud.formation(api)
+   Cloud.data(fff)[], Hostingcloud.
+<Typer>
+       Api cloud="hostingdata(local.server)
+@$HostingCloud="Extension.loader(json.api)
+            Extension <format>.data(complex.ttt)
+
+@$HostingName="Extension.api.php(Localhost:81.data(cloud.api)"
+@$ServerAmongus="Extension.api.php(localserver
+@$AmongusIP="HostingIP-cloud.cc(xml.data)-local.data)
+     
