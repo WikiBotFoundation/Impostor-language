af0337472f9eed82fda26db3ed1c120cb603ae8b
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-24 12:51:21 +0300

    Update

--- AmongUsData/LocalAmong.cs
+++ AmongUsData/LocalAmong.cs
@@ -5,3 +5,13 @@
                Dev.data/nanocomplex.java/data.yml(hosting.data.script="true"
          
            CreateScript.ip(data.script.command/local.github(localink.datascripter.script
+           CreateData.ip(data.script.command/local.yml(script.data(ishsb.)data.scripter 
+      Aminodata.script.data.lexer(local.data.scripter.
+            Scriptdata == Among us Launcher.cs
+         Creator.data(script.(data complex.js
+     Data.js(script.data.command(data="true"
+                      Among == launcher.data(script.data[], launcherdata.xml.data.
+           Locals.amongus.programming(local.data[],script.]]
+           Locals.amongus == scriptprogramming.data.killbutton.ejected.kill(Crewmate).data.complex
+           Locals.amongus == scripter data="true"
+
