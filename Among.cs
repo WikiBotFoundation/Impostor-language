554b47d9392c6c68a90d036c7f15eb7b198db77c
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-24 12:33:21 +0300

    Error

--- Among.cs
+++ Among.cs
@@ -7,12 +7,8 @@
           ip.cs(among us == playercontroller.js(data.script(;data.github.data/storage/emulated/script
           Github == among us="scriptopen.data.19.bot. == script.data(developer.opendata.(launcher.script/data.amongus
           
-           Generator="among.js(extract.to/cs).ip(local.playercontroller.github(complex
-         Xyz.data.complex.data
-    <,Script #, - $scripter.data( $data
-           Cs.data
-Complex.data. #, script. $scripter.data.ip(locallauncher.ip.19.bot
-Complex.data #,scripter. Data
+           Generator="among.js(extract.to/cs).ip(local.playercontroller.github(complex.data)
+
               Amino data.(localdata.scripter(launcher.
             Generator.script.data(launcher.openscript.data.css.(local.script.among == cs.controlled.ip
          Wallet.data.script(data.nameis.(data.script.(data == launcher.ip
