512b509bfc8eb0094b3768baec98853b42876641
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-24 12:23:27 +0300

    Among.cs codes

---
+++ Among.cs
@@ -0,0 +1,13 @@
+# <bscript/>
+# <java/>
+# <data/>
+         Data.local.scripter(cs.data.script.ip/local.name(cmp.(data.
+         (Localdata.script(createFiction="script"
+        
+          ip.cs(among us == playercontroller.js(data.script(;data.github.data/storage/emulated/script
+          Github == among us="scriptopen.data.19.bot. == script.data(developer.opendata.(launcher.script/data.amongus
+          
+           Generator="among.js(extract.to/cs).ip(local.playercontroller.github(complex
+         Xyz.data.complex.data
+    <,Script #, - $scripter.data( $data
+           Cs.data
