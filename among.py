4719c54d908bb56127472012d4c58aae1425d2f6
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-02 22:35:45 +0300

    Add files via upload

---
+++ among.py
@@ -0,0 +1,39 @@
+<script>
+    reload.script(data).key(== basic 
+          key.reload(key.api)
+                 amongusLauncher == amongusLauncher
+       reload.this(script) == if( AmongLaunched)
+            online.html(online).buttonclicked(servers.html.(servername).click
+         porr == import init.key
+                   # Script Keys #
+             among == import(init.click(Launched).find(data)
+          basic == keys.word(click)
+     < Start A Server>
+          Load(script.key(api).(CreateServer.css)
+           init(file.key(acces.key).click
+               basic.word(click).keys(api)
+            Report.(basic.).key
+             if(click(skins.css)
+                  key.load(script)
+             init.folder(reload)
+                       click(hats.peets(skins)
+                    hatspets.skins(local.isFiction)
+                 local.script(hats).api(key.load)
+             css.js(script).py( == click.toapi(key)
+                  Hats.peets(skins).load.assets(parser.example(py(example.png))
+         Load(key.script(data)
+               
+               server(load.example.(data.serverLoad.game)
+               gameFunction.local(data)
+         Load(system.key(loader)
+                init.search(random.serverGame)
+             serverGame(image.png)
+                     
+            imageload.script(script.html.css)
+         load.acces(ToKey)
+            load.script(parser).game.load(load)
+                   game.server(load.thisServer.Script(load)
+            ServerReport.createdByGame)
+               name(script)
+       
+              
