29d6e3810eb2174ebe3bf7f6621ebd112f265d8c
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-14 20:48:25 +0300

    Create Language.cs

---
+++ Language.cs
@@ -0,0 +1,37 @@
+get.js
+get.player
+get.monobhe
+   ( == PlayerFunc.(== server
+     const.server
+        PlayerMovement
+          player.server((hostname( == local)
+     
+       Host
+        server.host
+         local == host.name
+      getPlayerServer
+         if( == movement.install
+
+      private float == install.host
+      public float f++.install
+         install < 0 <; language
+            public.host
+      public float == install.hostname
+      movement.float == language.name
+         Home.install
+            host( == name.float
+      public float <0<0-val > language.host
+      public.host == (install.val
+         language.val
+              public.basic(host; val)
+            movement.val( <host>/<0>
+            install.val( <host>/name>
+      public float <0_abort.name
+             basic.val
+                 lang.aborted<hostname<0,0>
+         script.val(languageName="Impostor"
+            
+           input.commands == else.import
+         
+
+      
