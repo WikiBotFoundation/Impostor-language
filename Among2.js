579094a7a30c731f894d93f1bb528e820beb21eb
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-16 22:40:00 +0300

    Create Among2.js

---
+++ Among2.js
@@ -0,0 +1,33 @@
+<script>
+  MyCustomServer; my.data
+  MyCustomServer; json( ==> server.json
+     myJson.my( ==> data
+  MyCustomServer; ip; 0.0.0.0
+  YourServer;  php
+      MyJson ==> 02737
+    mov.custom
+       json.element ==> 0.93820
+       MyJs ==> 0282
+   MyCustomServer; element.ip
+    Element.text(5-php)
+      tex.json( == 017.0191
+     float.ip
+         float == 0.2018931
+         float == 0.0173639
+      Among.js(==> 928
+          MyCustom.server{=[hector.cilent}
+      {cilent.system[.it
+       if(custom{==> cilent.id ==>
+       create.cilent(Id ==> myAmongUsServer
+    client.javascript(Id ==> language
+        custom.cilent(Id ==> client.id ==> among.server
+        custom.server(Id ==> among.server({client.id)
+
+
+
+     
+
+
+
+ 
+   
