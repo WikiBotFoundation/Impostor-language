7628c7c7b627800bf4f20e11af2296badb156334
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-17 13:11:27 +0300

    Update Language.cs

--- Language.cs
+++ Language.cs
@@ -32,6 +32,51 @@ get.monobhe
          script.val(languageName="Impostor"
             
            input.commands == else.import
-         
+         input.server.val(import.impostor)
+         abort.lang(host(
+            isHost( ==> func.val(028)
+            createHost( ==> game.server
+        isHost.impostor
+             game.server(host.name)
+    game.server(==> zap.host
+   game.command(==> host.val
+       host(=site
+           private float ==> impostor
+       MyBox ==>
+        MyBox.server(input.command)
+      MyBox.game 
+           MyBox="input.func"
+             float ==> myBox.impostor
+      Server.float ==> createServer
+             GetIP ==> skeld.net
+          GETIP ==> server({MyBox.lang
+      getIP.float-token
+           isHost.name
+       Get.json(KysUewoVeoa)
+              local.host(000; server)
+    thisIp.server
+          localhost;(connect.system( == yourserver
+    YourServer.host ==>
+         Connect.system(; localhost
+         Connect.system(; ip.host
+    Your.server ==>
+         Connect.json(; abort.json
+     local.host(
+          Server.getIPJson
+              local.host
+     your.connectedToServer)
+             LanguageComment-//-
+           Connect.system
+       CreateCommand.system(this.system( ==> connectAmongUsServer="ip-name"
+      Command.ip(Name(connect.system
+        ip.json(;000
+            yourServer.connected
+     EditAppName="AmongUs.exe"
+         Among.us(player.host
+         edit(host-connected(; host
+      host.vpn(;name.exe
+               yourConnect.vpn
+
+
 
       
